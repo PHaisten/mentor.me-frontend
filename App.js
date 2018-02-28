6fd419d866fc27c6cfc87620a27628b58cac5279
Covalence Projects <paytonhaisten@gmail.com>
2018-02-28 10:58:45 -0600

    Initial Commit

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default class App extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={{ flex: 1, backgroundColor: 'orange' }} />
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF'
+	}
+});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,2 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
