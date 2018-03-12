1f2bc4a0a4a18b6a0fe80045d818c758dac5ffce
Covalence Projects <paytonhaisten@gmail.com>
2018-03-12 18:22:25 -0500

    "Changed Login Screens"

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import * as loginService from '../src/services/logInOut';
 import {
 	ScrollView,
 	TextInput,
@@ -13,6 +14,56 @@ import {
 import { Item, Icon } from 'native-base';
 
 export default class LoginScreen extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			//redirectToReferrer: false,
+			email: '',
+			password: ''
+			//feedbackMessage: '',
+			//checkingLogin: true
+		};
+	}
+
+	// componentDidMount() {
+	//     loginService.checkLogin()
+	//     .then((loggedIn) => {
+	//         if (loggedIn) {
+	// 			//{() => {this.props.screenProps.handleChoice('homementee')}}
+	//             this.setState(
+	// 				{ redirectToReferrer: true, checkingLogin: false }
+	// 			);
+	//         } else {
+	//             this.setState(
+	// 				{ checkingLogin: false }
+	// 			);
+	//          }
+	//     });
+	// }
+
+	login() {
+		loginService
+			.login(this.state.email, this.state.password)
+			.then(() => {
+				this.setState({ redirectToReferrer: true });
+			})
+			.catch(err => {
+				if (err.message) {
+					this.setState({ feedbackMessage: err.message });
+				}
+			});
+		this.props.screenProps.handleChoice('homementee');
+	}
+
+	handleEmailChange(value) {
+		this.setState({ email: value });
+	}
+	handlePasswordChange(value) {
+		this.setState({ password: value });
+	}
+	//userType(userid){  if (mentee) then -> 'metees'; else--> 'mentors
+	//...await onPress={() => {this.props.screenProps.handleChoice('homementee')}}
+
 	render() {
 		const { navigate } = this.props.navigation;
 
@@ -32,20 +83,38 @@ export default class LoginScreen extends Component {
 						returnKeyType="next"
 						onSubmitEditing={() => this.passwordInput.focus()}
 						keyboardType="email-address"
+						ref={el => {
+							this.email = el;
+						}}
+						onChangeText={email => {
+							this.setState({ email });
+							console.log(this.state);
+						}}
 					/>
 					<TextInput
 						placeholder="Password"
 						style={styles.input}
 						secureTextEntry
 						returnKeyType="go"
-						ref={input => (this.passwordInput = input)}
+						ref={el => {
+							this.password = el;
+						}}
+						onChangeText={password => {
+							this.setState({ password });
+							console.log(this.state);
+						}}
 					/>
 					<Button
 						onPress={() => navigate('CreateAccountLand', { name: 'Account' })}
 						title="New here? Create an Account"
 					/>
 				</View>
-				<TouchableOpacity style={styles.button}>
+				<TouchableOpacity
+					style={styles.button}
+					onPress={() => {
+						this.login();
+					}}
+				>
 					<Text style={styles.buttonText}>LOGIN</Text>
 				</TouchableOpacity>
 			</KeyboardAvoidingView>
